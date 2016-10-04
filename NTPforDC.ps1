c9e73b2bb5356ba92a391c001b716e5acd6423f9
colepnc <cole@pointandclicktulsa.com>
2016-10-04 15:18:55 -0500

    working script
    tested on VM, works as intended

--- NTPforDC.ps1
+++ NTPforDC.ps1
@@ -1,6 +1,6 @@
 ﻿#######
 # Script for setting up NTP on a fresh DC on a Hyper-V host you want to join to the domain.
-# To-do: Test on a fake DC, swap from "reg" to powershell if possible, same for w32tm
+# To-do: swap from "reg" to powershell if possible, same for w32tm
 # Improvements?: Change the while loop to have a wait of an hour or longer, and make it send
 #                an email to me if it has failed rather than checking every 10 seconds?
 #######
@@ -12,16 +12,16 @@ $ErrorActionPreference = "Continue"
 Start-Transcript -path C:\NTP.txt -append
 
 # VM IC Partial disable (preserves checkpoint/backup restore time fix)
-reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\VMICTimeProvider /v Enabled /t reg_word /d 0
+reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\VMICTimeProvider /v Enabled /t reg_dword /d 0 /f
 
 # Add NTP servers
-reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /v NtpServer /t reg_sz /d "0.us.pool.ntp.org,0x1 1.us.pool.ntp.org,0x1 2.us.pool.ntp.org,0x1 3.us.pool.ntp.org,0x1"
+reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /v NtpServer /t reg_sz /d "0.us.pool.ntp.org,0x1 1.us.pool.ntp.org,0x1 2.us.pool.ntp.org,0x1 3.us.pool.ntp.org,0x1" /f
 
 # Set w32time type to NTP
-reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /v Type /t reg_sz /d NTP
+reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /v Type /t reg_sz /d NTP /f
 
 # Set poll time to 900 seconds (15 minutes) to prevent VM time drift
-reg add HKLM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient /v SpecialPollInterval /t reg_word /d 900
+reg add HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient /v SpecialPollInterval /t reg_dword /d 900 /f
 
 # Restart w32time to refresh settings
 Restart-Service w32time
